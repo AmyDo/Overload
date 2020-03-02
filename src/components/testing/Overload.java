2a2ab36d6d9fbb51c8a481b7a58c236734fd89be
Amy Do <ahd6901@rit.edu>
2020-03-02 15:04:32 +0700

    Overload need work(display, toggle, connect)

--- src/components/testing/Overload.java
+++ src/components/testing/Overload.java
@@ -4,6 +4,7 @@ import components.*;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.lang.reflect.Array;
 import java.util.*;
 
 /**
@@ -14,7 +15,8 @@ import java.util.*;
 public class Overload {
     HashMap<String, Component> hmap = new HashMap<String, Component>();
     ArrayList<String> commandList = new ArrayList<String>();
-    ArrayList<PowerSource> powerSourceArrayList= new ArrayList<>();
+    ArrayList<PowerSource> powerSourceArrayList = new ArrayList<>();
+    ArrayList<String> typeArray = new ArrayList<>();
 
 
     public static final int BAD_ARGS = 1;
@@ -57,7 +59,7 @@ public class Overload {
     private static Reporter Support;
 
 
-    public Overload(String filename, String filename2 ) {
+    public Overload(String filename, String filename2) {
         readFile(filename);
         readCommandFile(filename2);
 
@@ -70,13 +72,14 @@ public class Overload {
 //            return;
 //        } else {
 //            new Overload(args[0]).run();
-            new Overload("config4.txt", "config4_inA.txt");
-      //  }
+        new Overload("config4.txt", "config4_inA.txt");
+        //  }
     }
 
     public void run() {
 
     }
+
     public void readFile(String filename) {
         try (Scanner configFile = new Scanner((new File(filename)))) {
             int count = 0;
@@ -90,7 +93,7 @@ public class Overload {
             System.out.println(count + " components created.");
             System.out.println("Starting up the main circuit(s): ");
             System.out.println("PowerSource Home(draw 0): powering up");
-            for (PowerSource ps: powerSourceArrayList){
+            for (PowerSource ps : powerSourceArrayList) {
                 ps.engage();
             }
         } catch (FileNotFoundException e) {
@@ -104,18 +107,27 @@ public class Overload {
      * @param line to be proccessed
      */
     public void processLine(String[] line) {
-        if (line[0].equals("PowerSource")) {
-            hmap.put(line[1], new PowerSource(line[1]));
-            powerSourceArrayList.add(new PowerSource(line[1]));     //add all the power source to the arraylist=> to call display later on
-        } else if (line[0].equals("CircusBreaker")) {
-            hmap.put(line[1], new Appliance(line[1], hmap.get(line[2]), Integer.parseInt(line[3])));
-        } else if (line[0].equals("Outlet")) {
-            hmap.put(line[1], new Outlet(line[1], hmap.get(line[2])));
+        typeArray.add("Appliance");
+        typeArray.add("CircuitBreaker");
+        typeArray.add("Outlet");
+        typeArray.add("PowerSource");
+        if (!typeArray.contains(line[0])) {
+            Support.usageError(UNKNOWN_COMPONENT_TYPE);
         } else {
-            hmap.put(line[1], new Appliance(line[2], hmap.get(line[2]), Integer.parseInt(line[3])));
+            if (line[0].equals("PowerSource")) {
+                hmap.put(line[1], new PowerSource(line[1]));
+                powerSourceArrayList.add(new PowerSource(line[1]));     //add all the power source to the arraylist=> to call display later on
+            } else if (line[0].equals("CircusBreaker")) {
+                hmap.put(line[1], new Appliance(line[1], hmap.get(line[2]), Integer.parseInt(line[3])));
+            } else if (line[0].equals("Outlet")) {
+                hmap.put(line[1], new Outlet(line[1], hmap.get(line[2])));
+            } else {
+                hmap.put(line[1], new Appliance(line[2], hmap.get(line[2]), Integer.parseInt(line[3])));
+            }
         }
     }
-    public void readCommandFile( String filename) {
+
+    public void readCommandFile(String filename) {
         try (Scanner configFile = new Scanner((new File(filename)))) {
             int count = 0;
             while (configFile.hasNextLine()) {
@@ -124,45 +136,58 @@ public class Overload {
                 count++;
             }
 
-        }catch (FileNotFoundException e) {
+        } catch (FileNotFoundException e) {
             Support.usageError(FILE_NOT_FOUND);
         }
     }
+
     public void proccessCommand(String[] line) {
         commandList.add("connect");
         commandList.add("toggle");
         commandList.add("display");
         commandList.add("quit");
+        if (!commandList.contains(line[0])) {
+            Support.usageError(UNKNOWN_USER_COMMAND);
 
-        for(String i: commandList){
-            if (!line[0].equals(i)){
-                Support.usageError(UNKNOWN_USER_COMMAND);
-                break;
-            }
         }
-        if(line[0].equals("display")){
+        if (line[0].equals("display")) {
             System.out.println(" ?  -> display[]");
-            for(PowerSource ps: powerSourceArrayList){
+            for (PowerSource ps : powerSourceArrayList) {
                 ps.display();       //display the chain
             }
 
-        }else if(line[0].equals("toggle")){
-            System.out.println(" ?  -> toggle["+ line[1]+ "]");
+        } else if (line[0].equals("toggle")) {
+            System.out.println(" ?  -> toggle[" + line[1] + "]");
+            System.out.println(line[1]);
+            System.out.println(hmap.get(line[1]));
+            if (hmap.containsKey(line[1])){  //check if the
+                if (hmap.get(line[1]).getSwitchable()) {    //check if the component is Circuit breaker or Appliance.
+                    hmap.get(line[1]).toggle();
+                } else {
+                    Support.usageError(UNSWITCHABLE_COMPONENT);
+                }
+            }else{
+                Support.usageError(UNKNOWN_COMPONENT);
 
-            if(hmap.get(line[1]).getSwitchable()){    //check if the component is Circuit breaker or Appliance.
-                hmap.get(line[1]).toggle();
             }
 
 
-        }else if(line[0].equals("connect")){
+        } else if (line[0].equals("connect")) {
             System.out.println(" ?  -> connect[ ");
+            String[] newLine= new String[line.length-1];  //create new array
+            for( int i=0; i< newLine.length;i++){
+                newLine[i]=line[i+1];
+            }
+            if (hmap.containsKey(line[3])) {   //check if the source is already existed or not.
+                processLine(newLine);      //proccess the new component.
 
-        }else if(line[0].equals("quit")){
+            } else {
+                Reporter.usageError(UNKNOWN_COMPONENT);
+            }
+        } else if (line[0].equals("quit")) {
             System.exit(1);
         }
     }
-
-
 
 
 }
